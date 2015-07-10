c215c209011d990cc303aa6fb001620a17d00a8f
MalakMilhem <malak@ses.yu.edu.jo>
2015-07-10 12:00:12 +0300

    add

---
+++ style/menu.php
@@ -0,0 +1,27 @@
+<head>
+	<link rel="stylesheet" href="style.css" type="text/css"> 
+</head>
+
+<body>
+
+  <div id="menu" style="position:relative";>
+   <ul>
+    <li><a href="#" title="If you want to go Back to home page">Home</a></li>
+    <li><div class="gap"></div></li>
+    <li><a href="about_as.php" title="who we are !">About Us</a></li>
+    <li><div class="gap"></div></li>
+    <li><a href="#" title="if you have any educational question tell us">Ask your questions</a></li>
+    <li><div class="gap"></div></li>
+    <li><a href="#" title="what you should learn from this website">Output</a></li>
+    <li><div class="gap"></div></li>
+    <li><a href="#" title="solution of weekly question">Solution</a></li>
+    <li><div class="gap"></div></li>
+    <li><a href="#" title="contact us if you have any problem">Contact Us</a></li>
+   </ul>
+  </div>
+  </body>
+  
+  <style>
+
+
+</style>
